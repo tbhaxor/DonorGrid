16d0b153b17ba66989e02dbdf88289765f1be2c6
Gurkirat Singh <tbhaxor@gmail.com>
2021-07-07 10:58:20 +0530

    improvement (Configuration): replace " with '

--- Configuration/admin.py
+++ Configuration/admin.py
@@ -5,6 +5,6 @@ from .models import PaymentMethod
 # Register your models here.
 @admin.register(PaymentMethod)
 class PaymentMethodRegister(admin.ModelAdmin):
-    empty_value_display = "N/A"
-    list_display = ["id", "provider", "client_key"]
+    empty_value_display = 'N/A'
+    list_display = ['id', 'provider', 'client_key']
     pass
--- Configuration/models.py
+++ Configuration/models.py
@@ -4,9 +4,13 @@ from django.utils.translation import gettext_lazy as _
 
 class PaymentMethod(models.Model):
 	class PaymentProvider(models.TextChoices):
-		STRIPE = "stripe", _("Stripe")
-		PAYPAL = "paypal", _("PayPal")
-		RAZORPAY = "razorpay", _("RazorPay")
+		STRIPE = 'stripe', _('Stripe')
+		PAYPAL = 'paypal', _('PayPal')
+		RAZORPAY = 'razorpay', _('RazorPay')
+
+	class PaymentEnvironment(models.TextChoices):
+		DEVELOPMENT = 'dev', _('Development'),
+		PRODUCTION = 'prod', _('Production')
 
 	provider = models.CharField(max_length=15, choices=PaymentProvider.choices, default=PaymentProvider.STRIPE)
 	secret_key = models.CharField(max_length=150, null=False, default=None)
