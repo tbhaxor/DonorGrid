f3b28ab822165094fe23ad484d404e14da0c574c
Gurkirat Singh <tbhaxor@gmail.com>
2021-07-07 11:00:35 +0530

    improvement (Donation): replace " with '

--- Donation/admin.py
+++ Donation/admin.py
@@ -5,7 +5,7 @@ from .models import Donation
 # Register your models here.
 @admin.register(Donation)
 class DonationModel(admin.ModelAdmin):
-    list_display = ["donor", "package", "amount", "created_at"]
-    list_filter = ["created_at"]
-    sortable_by = ["created_at", "donor"]
+    list_display = ['donor', 'package', 'amount', 'created_at']
+    list_filter = ['created_at']
+    sortable_by = ['created_at', 'donor']
     pass
