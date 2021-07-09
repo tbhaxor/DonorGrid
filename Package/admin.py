8f94118cd90978f2f36e287c4120130bcadf2c03
Gurkirat Singh <tbhaxor@gmail.com>
2021-07-09 05:44:52 +0530

    improvement (Package): customize admin functionality

--- Package/admin.py
+++ Package/admin.py
@@ -1,10 +1,49 @@
 from django.contrib import admin
 from .models import Package
+from django.forms import ModelForm
 
 
-# Register your models here.
+class PackageForm(ModelForm):
+    def clean(self):
+        ri = self.cleaned_data.get('recurring_interval')
+        ru = self.cleaned_data.get('recurring_unit')
+        if ri and ru is None:
+            self.add_error('recurring_unit', 'The field is required')
+        elif ri == 'year' and ru > 1:
+            self.cleaned_data['recurring_unit'] = 1
+        elif ri == 'month' and ru > 12:
+            self.cleaned_data['recurring_unit'] = 12
+        pass
+    pass
+
+
 @admin.register(Package)
 class PackageModel(admin.ModelAdmin):
+    form = PackageForm
     empty_value_display = 'N/A'
-    list_display = ['name', 'amount', 'recurring_unit', 'recurring_interval']
+    list_display = ['name', 'amount', 'currency', 'recurring_unit', 'recurring_interval']
+
+    fieldsets = (
+        ('Package Details', {
+            'description': 'Set name and description for the package',
+            'fields': ('name', 'description',)
+        }),
+        ('Amount Details', {
+            'description': 'Set package amount and currency',
+            'fields': ('amount', 'currency')
+        }),
+        ('Recurring Packages', {
+            'classes': ('collapse', ),
+            'description': 'Make this package to collect recurring donations',
+            'fields': ('recurring_unit', 'recurring_interval')
+        })
+    )
+
+    def save_model(self, request, obj: Package, form, change):
+        # TODO: create the subscription plan
+        return super(PackageModel, self).save_model(request, obj, form, change)
+
+    def delete_model(self, request, obj):
+        # TODO: delete the subscription plan
+        return super(PackageModel, self).delete_model(request, obj)
     pass
