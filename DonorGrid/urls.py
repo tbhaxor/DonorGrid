e9af38fe4742501bda79369fd13cb5eb7a8f84e3
Gurkirat Singh <tbhaxor@gmail.com>
2021-07-13 10:34:14 +0530

    improvement (Urls): update django admin title

--- DonorGrid/urls.py
+++ DonorGrid/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'DonorGrid Management'
+admin.site.site_title = 'DonorGrid'
+admin.site.index_title = 'Dashboard'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('packages/', include('Package.urls', namespace='package')),
