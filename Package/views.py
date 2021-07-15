33f6737731421c606ca9c8034bbe4c6b7384d7a4
Gurkirat Singh <tbhaxor@gmail.com>
2021-07-15 15:45:18 +0530

    improvement (Package): simplify urls

--- Package/views.py
+++ Package/views.py
@@ -1,24 +1,9 @@
-from django.http import HttpRequest, JsonResponse, HttpResponseNotFound
-from django.views import View
+from django.http import JsonResponse
 from .models import Package
 
 
 # Create your views here.
-class AllView(View):
+def get_all(request):
+    packages = Package.objects.exclude(is_hidden=True).values('id', 'name', 'description', 'amount', 'currency')
+    return JsonResponse(data=list(packages), safe=False)
 
-    def get(self, request: HttpRequest):
-        packages = Package.objects.exclude(is_hidden=True).values()
-        return JsonResponse(data=list(packages), safe=False)
-    pass
-
-
-class SingleView(View):
-
-    def get(self, request: HttpRequest, pk: int):
-        try:
-            package = Package.objects.values().get(id=pk)
-            return JsonResponse(data=dict(package))
-        except Package.DoesNotExist:
-            return HttpResponseNotFound('Package with id %d not found' % pk)
-        pass
-    pass
