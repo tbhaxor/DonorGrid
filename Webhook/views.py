841433323d1cff9fc13cc9db73c2f9c7827f3d89
Gurkirat Singh <tbhaxor@gmail.com>
2021-07-18 02:39:52 +0530

    fix (Webhook): remove prints

--- Webhook/views.py
+++ Webhook/views.py
@@ -5,8 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 @csrf_exempt
 def paypal(request: HttpRequest):
-    print(request.POST)
-    print(request.body)
     return HttpResponse("OK")
 
 
