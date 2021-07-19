0a24a5087cb2ec4fe92ebfccdd8c051b97960e72
Gurkirat Singh <tbhaxor@gmail.com>
2021-07-19 14:44:37 +0530

    fix (deployment): user creation in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,12 @@
 set -e
 sleep 10
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth.models import User; user = User.objects.create_superuser(username='${DG_USER:-donorgrid}', email='${DG_EMAIL:-donorgrid@example.com}'); user.set_password('${DG_PASS:-donorgrid}'); user.is_superuser = True; user.save()"
+USER_COUNT=$(python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='${DG_USER:-donorgrid}').count())")
+if [[ $USER_COUNT -eq 0 ]]
+then
+  python manage.py shell -c "from django.contrib.auth.models import User; user = User.objects.create_superuser(username='${DG_USER:-donorgrid}', email='${DG_EMAIL:-donorgrid@example.com}'); user.set_password('${DG_PASS:-donorgrid}'); user.is_superuser = True; user.save()"
+else
+  echo User "${DG_USER:-donorgrid}" already exists
+fi
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
