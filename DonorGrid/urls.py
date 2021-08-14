69534e469d08708684284fb848e3e3b453534bf5
Gurkirat Singh <tbhaxor@gmail.com>
2021-08-15 03:14:33 +0530

    fix (DonorGrid): implement development env media url

--- DonorGrid/urls.py
+++ DonorGrid/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = 'DonorGrid Management'
 admin.site.site_title = 'DonorGrid'
@@ -30,3 +32,7 @@ urlpatterns = [
     path('webhooks/', include('Webhook.urls', namespace='webhook')),
     path('callbacks/', include('Callback.urls', namespace='callback'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    pass
