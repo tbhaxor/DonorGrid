700c9b07864e2e2e710da8c5ec172b477d769dc7
Gurkirat Singh <tbhaxor@gmail.com>
2021-08-15 01:40:33 +0530

    feature (Donorgrid): register admin_interface app

--- DonorGrid/settings.py
+++ DonorGrid/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
