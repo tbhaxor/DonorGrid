6ee9e2138dd143930e74cb7a7723a17779e09cdf
Gurkirat Singh <tbhaxor@gmail.com>
2021-11-12 11:31:46 +0530

    improve (Donation): change amount field type to float

--- Donation/models.py
+++ Donation/models.py
@@ -11,7 +11,7 @@ class Donation(models.Model):
     donor = models.ForeignKey(to=Donor, on_delete=models.SET_NULL, null=True, default=None)
     package = models.ForeignKey(to=Package, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    amount = models.DecimalField(max_digits=20, decimal_places=2, null=True)
+    amount = models.FloatField(validators=[MinValueValidator(0)], null=True)
     currency = models.CharField(max_length=3, default='USD', null=False, blank=True)
     txn_id = models.CharField(max_length=255, null=True, blank=False)
     provider = models.CharField(max_length=15, choices=PaymentMethod.PaymentProvider.choices, default=PaymentMethod.PaymentProvider.STRIPE)
